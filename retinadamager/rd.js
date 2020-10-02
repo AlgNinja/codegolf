5ec818d8776de0047a12a91c0572f7fdb8bd4f06
Chris <51767118+AlgNinja@users.noreply.github.com>
2020-10-02 09:37:40 -0700

    Update rd.js

--- retinadamager/rd.js
+++ retinadamager/rd.js
@@ -1,1 +1,2 @@
 s=true;setup=()=>(createCanvas(windowWidth, windowHeight),frameRate(20));windowResized=()=>resizeCanvas(windowWidth, windowHeight);draw=()=>s?(background(0, 0, 0),s=false):(background(255, 255,255),s=true)
+// 205 chars
